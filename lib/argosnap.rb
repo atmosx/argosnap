9b857e9f9d6555872c66d12eaebe43cbb277241e
Panagiotis Atmatzidis <atma@convalesco.org>
2014-05-06 23:24:07 +0200

    Removed uneeded lines

--- lib/argosnap.rb
+++ lib/argosnap.rb
@@ -4,7 +4,3 @@ require_relative File.expand_path("../argosnap/version", __FILE__)
 require_relative File.expand_path("../argosnap/install", __FILE__)
 require_relative File.expand_path("../argosnap/balance", __FILE__)
 require_relative File.expand_path("../argosnap/osxnotify", __FILE__)
-# require 'argosnap/install'
-# require 'argosnap/balance'
-# require 'argosnap/version'
-# require 'argosnap/osxnotify'
