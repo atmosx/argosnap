0f1bbfe2ca7de04733d4cc5b3ed589c7873db0f6
Panagiotis Atmatzidis <atma@convalesco.org>
2015-10-28 18:24:14 +0200

    Added tests (no mocks)

--- test/lib/argosnap/balance_test.rb
+++ test/lib/argosnap/balance_test.rb
@@ -2,15 +2,16 @@ require_relative '../../test_helper'
 
 describe Argosnap do
   before do
-    # @balance = Argosnap::Fetch.new.balance
-    @balance = 4.0888
-  end
-
-  it "balance is not nil" do
-    @balance.wont_be_nil
+    @balance = Argosnap::Fetch.new.balance
+    @threshold = Argosnap::Configuration.new.data[:threshold]
   end
 
   it "balance is a float" do
     @balance.must_be_kind_of Float
   end
+
+  it "threshold is a float" do
+    @threshold.must_be_kind_of (Fixnum || Float)
+  end
+
 end
